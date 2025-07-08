62c938d59dd2791668cd2201e987bfc4e6296cab
arpan-gt <arpan.main@outlook.com>
2025-07-08 10:41:09 +0530

    admin model implemented

---
+++ models/admin.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const adminSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+const Admin = mongoose.model("Admin", adminSchema);
+export default Admin;
