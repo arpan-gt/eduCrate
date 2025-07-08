f098b62468db913d77678af3b854092ee990d4d6
arpan-gt <arpan.main@outlook.com>
2025-07-08 10:45:10 +0530

    admin middleware added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import { userRouter } from "./routes/user.route.js";
+import { courseRouter } from "./routes/course.route.js";
+import { adminRouter } from "./routes/admin.route.js";
 dotenv.config();
 
 const app = express();
@@ -7,6 +10,7 @@ const PORT = process.env.PORT;
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
+app.use("/api/v1/admin", adminRouter);
 
 app.listen(PORT, () => {
   console.log(`listening to PORT ${PORT}`);
