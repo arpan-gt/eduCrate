022d32b6a60d1937a716f8b36f50c5006c6c6bb0
arpan-gt <arpan.main@outlook.com>
2025-07-16 19:40:26 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+import { userRouter } from "./routes/user.routes.js";
+import { adminRouter } from "./routes/admin.routes.js";
+import cors from "cors";
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
 
 app.listen(PORT, () => {
   console.log(`listening to PORT ${PORT}`);
