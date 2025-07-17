28e6e8c3f1fd262998f2b03b8aadcb18b5e03895
arpan-gt <arpan.main@outlook.com>
2025-07-17 17:38:43 +0530

    admin signin route created

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 const adminRouter = express.Router();
 import { Admin } from '../models/Admin.models.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 adminRouter.post("/signup", async (req, res) => 
   {
@@ -51,6 +52,53 @@ try{
   )
 }
 
+
+  });
+
+adminRouter.post("/signin",async(req,res)=>
+  {
+    const { email , password } = req.body;
+    if(!email || !password)
+      {
+        return res.status(400).json(
+          {
+            message:"All fields are required"
+          }
+        )
+      };
+   
+   const existingAdmin = await Admin.findOne({ email });
+   if(!existingAdmin)
+    {
+     return res.status(400).json(
+        {
+          message:"user not registered"
+        }
+      )
+    }
+  
+  const isPasswordCorrect= await bcrypt.compare(password,existingAdmin.password);
+  if(!isPasswordCorrect)
+  {
+    return res.status(401).json(
+      {
+        message:"Invalid credentials"
+      }
+    )
+  }
+
+const token = jwt.sign({ adminId : existingAdmin._id},process.env.JWT_ADMIN_SECRET,{expiresIn:"1h"});
+
+return res.status(200).json(
+  {
+    message:"Admin signin successfully",
+    token,
+    admin:{
+      id:existingAdmin._id,
+      userName:existingAdmin.userName
+    }
+  }
+)
 
   })
 export { adminRouter };
