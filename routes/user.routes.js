72866922f3d3767b41ee08928634cce29c3d9d07
arpan-gt <arpan.main@outlook.com>
2025-07-17 16:19:10 +0530

    user signin route created

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
 import express from "express";
 const userRouter = express.Router();
-import User from "../models/User.models"
+import { User } from "../models/User.models.js"
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 userRouter.post("/signup", async (req, res) => {
   const { email, password, userName } = req.body;
@@ -30,6 +31,9 @@ userRouter.post("/signup", async (req, res) => {
     return res.status(201).json(
       {
         message: "user registered successfully",
+        user: {
+          id: user._id
+        }
       }
     )
 
@@ -43,4 +47,60 @@ userRouter.post("/signup", async (req, res) => {
   }
 });
 
+
+userRouter.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json(
+      {
+        message: "all fields are required"
+      }
+    )
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(400).json(
+        {
+          message: "user not registered"
+        })
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json(
+        {
+          message: "invalid credentials"
+        }
+      )
+    }
+    const token = jwt.sign({ userId: existingUser._id }, process.env.JWT_USER_SECRET, { expiresIn: "1h" });
+
+    return res.status(200).json(
+      {
+        message: "login successfully",
+        token,
+        user: {
+          id: existingUser._id,
+          userName: existingUser.userName,
+          email: existingUser.email
+        }
+      }
+
+    )
+  } catch (err) {
+    return res.status(500).json(
+      {
+        message: "something went wrong",
+        error: err.message
+      }
+    )
+  }
+
+
+
+})
 export { userRouter };
