debfbbe8ffad7497b6ae01f51a92198c74b9687e
arpan-gt <arpan.main@outlook.com>
2025-07-17 17:39:39 +0530

    admin model created

--- models/Admin.models.js
+++ models/Admin.models.js
@@ -2,27 +2,27 @@ import mongoose from "mongoose";
 const adminSchema = new mongoose.Schema(
   {
     userName:
-      {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 2,
-      },
+    {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+    },
     email:
-      {
-        type: String,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        required:true
-      },
+    {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      required: true
+    },
     password:
-      {
-        type: String,
-        required:true
-      }
+    {
+      type: String,
+      required: true
+    }
   },
   { timestamps: true });
 
 const Admin = mongoose.model("Admin", adminSchema);
-export {Admin}
+export { Admin }
