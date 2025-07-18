ce5bb0ddb2bfd294961d65501fa2c1a1acf2d80b
arpan-gt <arpan.main@outlook.com>
2025-07-18 23:38:38 +0530

    created new routes to create course and update

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const adminRouter = express.Router();
-import { signin, signup } from "../controllers/admin.controllers.js";
+import { signin, signup, createCourse, updateCourse } from "../controllers/admin.controllers.js";
 
 adminRouter.post("/signup", signup);
 adminRouter.post("/signin", signin);
-adminRouter.post("/addCourse", async (req, res) => { })
+adminRouter.post("/createCourse", createCourse);
+adminRouter.put("/course/:id", updateCourse);
 
 export { adminRouter };
