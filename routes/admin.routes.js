95ca73092863fa7dedb0421de21ff94386839501
arpan-gt <arpan.main@outlook.com>
2025-07-19 15:27:10 +0530

    added get and delete routes for course

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 const adminRouter = express.Router();
-import { signin, signup, createCourse, updateCourse } from "../controllers/admin.controllers.js";
+import { signin, signup, createCourse, updateCourse, allCourses, deleteCourse } from "../controllers/admin.controllers.js";
 
 adminRouter.post("/signup", signup);
 adminRouter.post("/signin", signin);
 adminRouter.post("/createCourse", createCourse);
 adminRouter.put("/course/:id", updateCourse);
+adminRouter.get("/allCourses", allCourses);
+adminRouter.delete("/delete/:id", deleteCourse)
 
 export { adminRouter };
