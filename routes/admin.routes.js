03b95e58e37bbfe964a7895830f257f8c0a3a492
arpan-gt <arpan.main@outlook.com>
2025-07-19 18:24:13 +0530

    middleware for zod validation added

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 const adminRouter = express.Router();
+import { validateRequest } from "../middlewares/validateRequest.js";
+import { createCourseSchema, updateCourseSchema } from "../schemas/course.schemas.js";
+import { signinSchema, signupSchema } from "../schemas/auth.schemas.js";
 import { signin, signup, createCourse, updateCourse, allCourses, deleteCourse } from "../controllers/admin.controllers.js";
 
-adminRouter.post("/signup", signup);
-adminRouter.post("/signin", signin);
-adminRouter.post("/createCourse", createCourse);
-adminRouter.put("/course/:id", updateCourse);
+adminRouter.post("/signup", validateRequest(signupSchema), signup);
+adminRouter.post("/signin", validateRequest(signinSchema), signin);
+adminRouter.post("/createCourse", validateRequest(createCourseSchema), createCourse);
+adminRouter.put("/course/:id", validateRequest(updateCourseSchema), updateCourse);
 adminRouter.get("/allCourses", allCourses);
 adminRouter.delete("/delete/:id", deleteCourse)
 
