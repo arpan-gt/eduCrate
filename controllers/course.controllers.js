2a2822e33fd67d98e2a638d75883dce1121b80ed
arpan-gt <arpan.main@outlook.com>
2025-07-21 23:30:54 +0530

    course controller added

---
+++ controllers/course.controllers.js
@@ -0,0 +1,57 @@
+import { Course } from "../models/Course.model.js";
+import { Purchase } from "../models/Purchase.models.js";
+
+const purchase = async (req, res) => {
+  const userId = req.userId;
+  const { courseId } = req.body;
+
+  try {
+    const course = await Course.findById(courseId);
+    if (!course) {
+      return res.status(404).json({
+        message: "Course not found"
+      });
+    };
+
+    const existingCourse = await Purchase.findOne({ userId, courseId });
+    if (existingCourse) {
+      return res.status(409).json({
+        message: "Course already purchased"
+      })
+    };
+
+    const purchase = await Purchase.create({
+      userId,
+      courseId,
+      creatorId: course.creatorId
+    });
+
+    return res.status(201).json({
+      message: "Course purchased successfully",
+      purchase
+    });
+  } catch (err) {
+    console.error("Error purchasing course:", err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+
+const preview = async (req, res) => {
+
+  try {
+    const courses = await Course.find();
+
+    return res.status(200).json({
+      message: "Courses fetched successfully",
+      courses
+    })
+
+  } catch (err) {
+    console.error("Error fetching courses:", err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+
+}
+
+export { purchase, preview }
