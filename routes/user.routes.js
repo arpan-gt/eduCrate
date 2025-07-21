6b183399c97b878db92217a4babda0e412d7656c
arpan-gt <arpan.main@outlook.com>
2025-07-21 23:07:42 +0530

    user routes created

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 const userRouter = express.Router();
-import { signup, signin } from "../controllers/user.controllers.js";
+import { signup, signin, purchases } from "../controllers/user.controllers.js";
+import { userMiddleware } from "../middlewares/user.middleware.js";
+import { validateRequest } from "../middlewares/validateRequest.middleware.js";
 
-userRouter.post("/signup", signup);
-userRouter.post("/signin", signin);
-
+userRouter.post("/signup", validateRequest(signup), signup);
+userRouter.post("/signin", validateRequest(signin), signin);
+userRouter.get("/purchases", userMiddleware, purchases);
 
 export { userRouter };
