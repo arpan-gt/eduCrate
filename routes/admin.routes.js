4080f48565dd0ebece430fd2cb437683213c4b08
arpan-gt <arpan.main@outlook.com>
2025-07-21 23:09:58 +0530

    created new routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,15 +1,24 @@
 import express from "express";
 const adminRouter = express.Router();
-import { validateRequest } from "../middlewares/validateRequest.js";
+import { validateRequest } from "../middlewares/validateRequest.middleware.js";
 import { createCourseSchema, updateCourseSchema } from "../schemas/course.schemas.js";
 import { signinSchema, signupSchema } from "../schemas/auth.schemas.js";
-import { signin, signup, createCourse, updateCourse, allCourses, deleteCourse } from "../controllers/admin.controllers.js";
+import { adminMiddleware } from "../middlewares/admin.middleware.js";
+import {
+  signin,
+  signup,
+  createCourse,
+  updateCourse,
+  allCourses,
+  deleteCourse
+} from "../controllers/admin.controllers.js";
+
 
 adminRouter.post("/signup", validateRequest(signupSchema), signup);
 adminRouter.post("/signin", validateRequest(signinSchema), signin);
-adminRouter.post("/createCourse", validateRequest(createCourseSchema), createCourse);
-adminRouter.put("/course/:id", validateRequest(updateCourseSchema), updateCourse);
-adminRouter.get("/allCourses", allCourses);
-adminRouter.delete("/delete/:id", deleteCourse)
+adminRouter.post("/createCourse", validateRequest(createCourseSchema), adminMiddleware, createCourse);
+adminRouter.put("/course/:id", adminMiddleware, validateRequest(updateCourseSchema), updateCourse);
+adminRouter.get("/allCourses", adminMiddleware, allCourses);
+adminRouter.delete("/delete/:id", adminMiddleware, deleteCourse)
 
 export { adminRouter };
