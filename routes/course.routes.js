1a1aaa09ac2c00d9b7fba0c4c7eb927fe56bd593
arpan-gt <arpan.main@outlook.com>
2025-07-25 23:48:14 +0530

    previewOne route created

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { userMiddleware } from '../middlewares/user.middleware.js';
 import { adminMiddleware } from '../middlewares/admin.middleware.js';
-import { preview, purchase } from '../controllers/course.controllers.js';
+import { preview, previewOne, purchase } from '../controllers/course.controllers.js';
 const courseRouter = express.Router();
 
 courseRouter.post("/purchase", userMiddleware || adminMiddleware, purchase);
 courseRouter.get("/preview", preview);
+courseRouter.get("/:id", previewOne);
 
 export { courseRouter }
